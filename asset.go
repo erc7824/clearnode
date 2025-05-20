6671e541e3be677ccf867830415ff65d4c1cdec2
Dmytro Steblyna <dsteblyna@openware.center>
2025-05-20 09:02:14 +0100

    use string for token address

--- asset.go
+++ asset.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"github.com/ethereum/go-ethereum/common"
 	"gorm.io/gorm"
 )
 
 type Asset struct {
-	Token    common.Address `gorm:"column:token;primaryKey"`    // part of primaryKey
-	ChainID  uint32         `gorm:"column:chain_id;primaryKey"` // part of primaryKey
-	Symbol   string         `gorm:"column:symbol;index"`        // e.g. "usdc"
-	Decimals uint8          `gorm:"column:decimals;not null"`
+	Token    string `gorm:"column:token;primaryKey"`    // part of primaryKey
+	ChainID  uint32 `gorm:"column:chain_id;primaryKey"` // part of primaryKey
+	Symbol   string `gorm:"column:symbol;index"`        // e.g. "usdc"
+	Decimals uint8  `gorm:"column:decimals;not null"`
 }
 
 func (Asset) TableName() string {
