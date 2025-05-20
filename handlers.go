7b738423769b008e1895b2ce621eb7ca1e805c7b
Dmytro Steblyna <dsteblyna@openware.center>
2025-05-20 10:31:01 +0100

    add logs

--- handlers.go
+++ handlers.go
@@ -613,13 +613,16 @@ func HandleResizeChannel(rpc *RPCMessage, db *gorm.DB, signer *Signer) (*RPCMess
 	if err != nil {
 		return nil, fmt.Errorf("failed to check participant A balance: %w", err)
 	}
+	fmt.Println("unified balance:", balance.String())
 	rawBalance := balance.Shift(int32(asset.Decimals)).BigInt()
+	fmt.Println("unified balance raw:", rawBalance.String())
 
 	newChannelAmount := new(big.Int).Add(new(big.Int).SetUint64(channel.Amount), params.AllocateAmount)
 	if rawBalance.Cmp(newChannelAmount) < 0 {
 		return nil, errors.New("insufficient unified balance")
 	}
-
+	fmt.Println("channel amount:", channel.Amount)
+	fmt.Println("new channel amount:", newChannelAmount.String())
 	newChannelAmount.Add(newChannelAmount, params.ResizeAmount)
 
 	if newChannelAmount.Cmp(big.NewInt(0)) < 0 {
